changed linreg main, fixed inconsistencies, target scaling

main_LinReg.py
@@ -34,7 +34,7 @@ from utils_LinReg import *
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Experiment Inputs')
-    parser.add_argument('--dataset', help='Dataset', type=str, default='adult')
+    parser.add_argument('--dataset', help='Dataset', type=str, default='linregbinary')
     parser.add_argument('--method', help='Method to be used', type=str, default='aim',
                         choices=['public', 'adassp', 'aim'])
     parser.add_argument('--delta', type=float, default=1e-5)
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     parser.add_argument('--iterate_over_gamma', action=argparse.BooleanOptionalAction,
                         default=False)
     parser.add_argument('--one_hot', action=argparse.BooleanOptionalAction,
-                        default=True)
+                        default=False)
     args = parser.parse_args()
 
     dataset = args.dataset
@@ -56,7 +56,8 @@ if __name__ == "__main__":
     n_limit = args.n_limit
     train_ratio = args.train_ratio
     one_hot = args.one_hot
-    rescale = True
+    print(one_hot)
+    rescale = one_hot # if one hot encoding is active, always rescale features
 
     marginals_pgm = 'all-pairs'
     target_dict = {'adult': 'education-num', 'ACSincome-LIN': 'PINCP',
@@ -79,6 +80,14 @@ if __name__ == "__main__":
                                                                          n_limit, train_ratio, one_hot)
 
 
+    # print("X", X)
+    # print("\n")
+    # print("y", y)
+    # print("\n")
+    # print("X_test", X_test)
+    # print("TARGET:", target)
+
+
     # Set up output and pbar
     res_out = []
     col_out = ['dataset', 'method', 'mse', 'r2', 'experiment_n', 'seed', 'n_limit', 'train_ratio',
@@ -94,6 +103,11 @@ if __name__ == "__main__":
             np.random.seed(seed + t)
 
             if method == 'public':
+
+                # X_public = copy.deepcopy(X)
+                # X_test_public = copy.deepcopy(X_test)
+                # y_public = copy.deepcopy(y)
+                # y_test_public = copy.deepcopy(y_test)
 
                 theta_public, mse_public, r2_public = public_method(X, y, X_test, y_test)
 
@@ -119,6 +133,8 @@ if __name__ == "__main__":
                              f'{seed}seed_W.pickle'
                 with open(W_filename, 'wb') as handle:
                     pickle.dump(W, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+                print(training_columns)
 
                 theta_aim, mse_aim, r2_aim = dp_query_ss_method(W_expanded, attribute_dict, training_columns, target,
                                                                 cols_to_dummy, one_hot, X_test, y_test)
@@ -157,8 +173,8 @@ if __name__ == "__main__":
                 #     X_test_adassp = copy.deepcopy(X_test)
                 #     y_adassp = copy.deepcopy(y)
 
-                bound_XTX, bound_X = get_bound_XTX(attribute_dict, target, cols_to_dummy, one_hot, rescale)
-                bound_y = np.abs(np.max(attribute_dict[target]))
+                bound_XTX, bound_X = get_bound_XTX(attribute_dict, target, cols_to_dummy, one_hot)
+                bound_y = 1 if one_hot else np.abs(np.max(attribute_dict[target]))
                 print(f"bound_y {bound_y}")
                 print(f"bound_XTX {bound_XTX}")
                 print(f"bound_X {bound_X}")
@@ -166,10 +182,7 @@ if __name__ == "__main__":
                 # adassp method
                 theta_adassp, mse_adassp, r2_adassp = AdaSSP_linear_regression(X, y, epsilon, delta,
                                                                                rho, bound_X, bound_y, bound_XTX,
-                                                                               X_test, y_test,
-                                                                               [min(attribute_dict[target]),
-                                                                                max(attribute_dict[target])],
-                                                                               rescale=rescale)
+                                                                               X_test, y_test)
 
                 print(f"theta adassp: {theta_adassp}")
                 print("mse adassp", mse_adassp)

main_LogReg.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Experiment Inputs')
     parser.add_argument('--dataset', help='Dataset', type=str, default='ACSincome')
-    parser.add_argument('--method', help='Method to be used', type=str, default='aim',
+    parser.add_argument('--method', help='Method to be used', type=str, default='genobjpert',
                         choices=['public', 'diffprivlib', 'aim', 'genobjpert'])
     parser.add_argument('--delta', type=float, default=1e-5)
     parser.add_argument('--num_experiments', type=int, default=1)
@@ -170,6 +170,8 @@ if __name__ == "__main__":
         X = X_ohe.copy()
         X_test = X_test_ohe.copy()
 
+    encoded_features = [col.split(".")[0] for col in X if col.split("_")[0] in cols_to_dummy]
+
     # print(f'Max Sensitivity: {max([np.linalg.norm(row) for row in X.to_numpy()])}')
 
     for t in range(num_experiments):
@@ -182,7 +184,8 @@ if __name__ == "__main__":
                 (public_f1score, public_accuracy,
                  public_fpr, public_tpr,
                  public_threshold, public_auc) = public_method(X, y, X_test, y_test,
-                                                               cols_to_dummy, attribute_dict, one_hot, approx = False)
+                                                               cols_to_dummy, attribute_dict,
+                                                               encoded_features, one_hot, approx = False)
                 res_out.append([dataset, method, public_auc, public_f1score, public_accuracy, t, seed,
                                 n_limit, train_ratio, None, epsilon])
                 print(f"public auc reg: {public_auc}")
@@ -216,8 +219,7 @@ if __name__ == "__main__":
                 # with open(filename, 'rb') as handle:
                 #     W = pickle.load(handle)
 
-                ######## AIM SS #########
-                encoded_features = [col.split(".")[0] for col in X if col.split("_")[0] in cols_to_dummy]
+                ######## AIM SS ########
 
                 (DPapprox_f1score, DPapprox_accuracy,
                  DPapprox_fpr, DPapprox_tpr,

methods_LinReg.py
@@ -34,7 +34,18 @@ from utils_LinReg import *
 
 def public_method(X, y, X_test, y_test):
 
-    theta_public, mse_public, r2_public = public_linear_regression(X, y, X_test, y_test)
+    # XTX = np.dot(X.T, X)
+    # XTy = np.dot(X.T, y)
+    # theta_public = np.linalg.solve(XTX, XTy)
+    # y_pred = np.dot(X_test, theta_public)
+    # mse_public = mean_squared_error(y_test, y_pred)
+
+    regr = LinearRegression(fit_intercept=False)
+    regr.fit(X.values, y.values)
+    theta_public = regr.coef_
+    y_pred = regr.predict(X_test.values)
+    mse_public = mean_squared_error(y_test, y_pred)
+    r2_public = r2_score(y_test, y_pred)
 
     return (theta_public, mse_public, r2_public)
 
@@ -59,16 +70,12 @@ def dp_query_ss_method(W_expanded, attribute_dict, training_columns, target,
     XTX = ZTZ.loc[training_columns, training_columns]
     XTy = ZTZ.loc[training_columns, target]
 
-    print(XTX)
-    print(ZTZ)
-
     for i,col in enumerate(XTX.columns):
-        print(col, X_test.columns[i])
         assert col == X_test.columns[i]
 
     # get estimator
-    theta_query_ss = np.linalg.solve(XTX, XTy)
-    y_pred = np.dot(X_test, theta_query_ss)
+    theta_query_ss = np.linalg.solve(XTX, XTy)    # not rescaled
+    y_pred = np.dot(X_test, theta_query_ss)       # not rescaled
     mse_query_ss = mean_squared_error(y_test, y_pred)
     r2_query_ss = r2_score(y_test, y_pred)
 
@@ -111,14 +118,13 @@ def dp_query_synth_data_method(synth_X, synth_y, training_columns, cols_to_dummy
     for i,col in enumerate(synth_X_ordered.columns):
         assert col == X_test_aim.columns[i]
 
-    theta_aim_synth, mse_aim_synth, r2_aim_synth = public_linear_regression(synth_X_ordered, synth_y,
+    theta_aim_synth, mse_aim_synth, r2_aim_synth = public_method(synth_X_ordered, synth_y,
                                                                             X_test_aim, y_test)
 
     return (theta_aim_synth, mse_aim_synth, r2_aim_synth)
 
 
-def AdaSSP_linear_regression(X, y, epsilon, delta, rho, bound_X, bound_y, bound_XTX, X_test, y_test, original_y_range,
-                             rescale):
+def AdaSSP_linear_regression(X, y, epsilon, delta, rho, bound_X, bound_y, bound_XTX, X_test, y_test):
     """Returns DP linear regression model and metrics using AdaSSP. AdaSSP is described in Algorithm 2 of
         https://arxiv.org/pdf/1803.02596.pdf.
 
@@ -165,11 +171,6 @@ def AdaSSP_linear_regression(X, y, epsilon, delta, rho, bound_X, bound_y, bound_
     theta_dp = np.linalg.solve(XTX_dp_reg, XTy_dp)
 
     y_pred = np.dot(X_test, theta_dp)
-
-    # # scale y pred back to original domain if rescale needed. Linear rescaling.
-    # if rescale:
-    #     y_pred = (y_pred - (-1)) / (+1 - (-1)) * (original_y_range[1] - original_y_range[0]) + original_y_range[0]
-    #     y_pred = y_pred.astype(int)
     mse_dp = mean_squared_error(y_test, y_pred)
     r2_dp = r2_score(y_test, y_pred)
 

utils.py
@@ -86,7 +86,7 @@ def preprocess_data(dataset, target_dict, n_limit, train_ratio, one_hot):
     print(attribute_dict)
 
     # If one_hot is active, then we one hot both the train set and the test set.
-    cols_to_dummy, training_columns = [], None
+    cols_to_dummy, training_columns = [], X.columns
     if one_hot:
         print(f"one-hot encoding {dataset}...")
         cols_to_dummy = get_cols_to_dummy(dataset)
@@ -105,6 +105,7 @@ def preprocess_data(dataset, target_dict, n_limit, train_ratio, one_hot):
         X = X_ohe.copy(deep=True)
         print("X after 1h:", X)
         X_test = X_test_ohe.copy()
+
 
     encoded_features = [col for col in X if col.split("_")[0] in cols_to_dummy]
     original_ranges = {feature: [0, domain[feature]] for feature in attribute_dict.keys()}
@@ -197,16 +198,14 @@ def selectTargetMarginals(cols, target, mode='target-pairs'):
     return out
 
 
-def get_bound_XTX(attribute_dict, target, features_to_encode, one_hot, rescale):
+def get_bound_XTX(attribute_dict, target, features_to_encode, one_hot):
 
     if not one_hot: # then data is binary synthetic data
         bound_X = np.sqrt(np.sum([max(attribute_dict[f])**2 for f in attribute_dict if f!=target]))
         bound_XTX = bound_X**2
-    elif one_hot and rescale:
+    elif one_hot:
         bound_XTX = len(attribute_dict.keys()) - 1    #excludes target
         bound_X = np.sqrt(bound_XTX)
-    elif one_hot and not rescale:
-        print("this option is not covered")
 
     return bound_XTX, bound_X
 
@@ -357,7 +356,6 @@ def phi_logit(x):
 
 def logit_2(x):
     return math.log(1 + math.exp(x))
-
 
 
 def get_ZTZ(W, attribute_dict, columns, features_to_encode, rescale):