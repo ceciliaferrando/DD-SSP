fixed y-scaling parameter for ZTZ

main_lin.py
@@ -43,12 +43,12 @@ from private_pgm_local.src.mbi.workload import Workload
 from utils import *
 
 parser = argparse.ArgumentParser(description='Experiment Inputs')
-parser.add_argument('--dataset', help='Dataset', type=str, default='adult')
+parser.add_argument('--dataset', help='Dataset', type=str, default='fire')
 parser.add_argument('--method', help='Method to be used', type=str, default='aim',
                     choices=['public', 'adassp', 'aim'])
 parser.add_argument('--delta', type=float, default=1e-5)
-parser.add_argument('--num_experiments', type=int, default=1)
-parser.add_argument('--seed', type=int, default=239)
+parser.add_argument('--num_experiments', type=int, default=5)
+parser.add_argument('--seed', type=int, default=22)
 parser.add_argument('--n_limit', type=int, default=50000)
 parser.add_argument('--one_hot', type=str, default='True')
 parser.add_argument('--scale_y', type=str, default='True')
@@ -102,6 +102,8 @@ if __name__ == "__main__":
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
+    models_dir = "models_linear"
+
 
     for t in range(num_experiments):
 
@@ -114,11 +116,7 @@ if __name__ == "__main__":
             bound_y = 1 if one_hot else np.abs(np.max(attribute_dict[target]))
 
             XTX_public = pd.DataFrame(np.dot(X.T, X), index=X.columns, columns=X.columns)
-            print(XTX_public)
             XTy_public = pd.DataFrame(np.dot(X.T, y), index=X.columns)
-
-            print(f"X bound {bound_X}")
-            print(f"y bound {bound_y}")
 
             if method == 'public':
                 theta_public = public_linreg(X, y)
@@ -148,8 +146,8 @@ if __name__ == "__main__":
                 for aim_y_mrg_opt in [False]:
 
                     file_id = f'{dataset}_epsilon{epsilon}_delta{delta}_nlimit{n_limit}_t{t}_aimopt{aim_y_mrg_opt}'
-                    if os.path.exists(f'models/aim_model_{file_id}.pkl'):
-                        with open(f'models/aim_model_{file_id}.pkl', 'rb') as f:
+                    if os.path.exists(f'{models_dir}/aim_model_{file_id}.pkl'):
+                        with open(f'{models_dir}/aim_model_{file_id}.pkl', 'rb') as f:
                             aim_model_graph = dill.load(f)
                             mrgs = selectTargetMarginals(pgm_train_df.columns, target, mode=PGMmarginals)
                             workload = {(m, 1.0) for m in mrgs}
@@ -161,11 +159,11 @@ if __name__ == "__main__":
                         aim_model_graph, workload = get_aim_model(pgm_train_df, domain, target, PGMmarginals, epsilon, delta, model_size,
                                                             max_iters, len(X), initial_cliques)
 
-                        with open(f'models/aim_model_{file_id}.pkl', 'wb') as f:
+                        with open(f'{models_dir}/aim_model_{file_id}.pkl', 'wb') as f:
                             dill.dump(aim_model_graph, f)
 
                         # 2) load AIM model and get marginal tables and synthetic data X_synth, y_synth
-                        with open(f'models/aim_model_{file_id}.pkl', 'rb') as f:
+                        with open(f'{models_dir}/aim_model_{file_id}.pkl', 'rb') as f:
                             aim_model_graph = dill.load(f)
 
                     print(workload)
@@ -222,16 +220,16 @@ if __name__ == "__main__":
                     # PGM direct prediction
                     G = aim_model_graph
                     target_levels = attribute_dict[target]
-                    G_y = X_test_pre.apply(pred_y_given_x_from_G, axis=1, args=(G, target, target_levels))
+                    G_y = X_test_pre.apply(pred_y_given_x_from_G, axis=1, args=(G, target, target_levels, 'linear'))
                     G_y = G_y.to_frame(name=target)
                     if scale_y:
                         G_y = normalize_minus1_1(G_y, attribute_dict, encoded_features)
                     G_mse = mean_squared_error(y_test, G_y)
-                    print(G_mse)
+                    print("G mse", G_mse)
                     res_out.append([dataset, "aim_G_init" + str(aim_y_mrg_opt), G_mse, None, t, seed,
                                     n_limit, None, epsilon, delta])
 
             out_df = pd.DataFrame(res_out, columns=col_out)
             filename_out = f'{outdir}{dataset}_{method}_{epsilon}_{t}_{num_experiments}exps_{n_limit}limit_' \
-                           f'{seed}seed_aim_opt{aim_y_mrg_opt}.csv'
+                           f'{seed}seed.csv'
             out_df.to_csv(filename_out)

main_log.py
@@ -43,16 +43,16 @@ from private_pgm_local.src.mbi.workload import Workload
 from utils import *
 
 parser = argparse.ArgumentParser(description='Experiment Inputs')
-parser.add_argument('--dataset', help='Dataset', type=str, default='ACSPublicCoverage')
-parser.add_argument('--method', help='Method to be used', type=str, default='genobjpert',
+parser.add_argument('--dataset', help='Dataset', type=str, default='ACSincome')
+parser.add_argument('--method', help='Method to be used', type=str, default='aim',
                     choices=['public', 'genobjpert', 'aim'])
 parser.add_argument('--delta', type=float, default=1e-5)
 parser.add_argument('--num_experiments', type=int, default=5)
-parser.add_argument('--seed', type=int, default=240)
+parser.add_argument('--seed', type=int, default=242)
 parser.add_argument('--n_limit', type=int, default=50_000)
 parser.add_argument('--one_hot', type=str, default='True')
 parser.add_argument('--scale_y', type=str, default='False')
-parser.add_argument('--aim_y_mrg_opt', type=str, default='True')
+parser.add_argument('--aim_y_mrg_opt', type=str, default='False')
 
 args = parser.parse_args()
 
@@ -64,8 +64,8 @@ num_experiments = args.num_experiments
 seed = args.seed
 n_limit = args.n_limit
 one_hot = True if args.one_hot == 'True' else False
-scale_y = True if args.scale_y == 'True' else False
-# aim_y_mrg_opt = True if args.aim_y_mrg_opt == 'True' else None
+scale_y = False
+aim_y_mrg_opt = False
 
 if __name__ == "__main__":
 
@@ -86,6 +86,8 @@ if __name__ == "__main__":
                                                                     one_hot, scale_y)
 
 
+
+
     n, d = X.shape
 
     print(f"X.shape {X.shape}")
@@ -99,6 +101,8 @@ if __name__ == "__main__":
     outdir = dataset + "_logreg/"
     if not os.path.exists(outdir):
         os.makedirs(outdir)
+
+    models_dir = "models_logistic"
 
 
     for t in range(num_experiments):
@@ -133,95 +137,91 @@ if __name__ == "__main__":
 
             elif method == 'aim':
 
-                for aim_y_mrg_opt in [False]:
+                file_id = f'{dataset}_epsilon{epsilon}_delta{delta}_nlimit{n_limit}_t{t}_ymrgs{aim_y_mrg_opt}'
+                if os.path.exists(f'{models_dir}/aim_model_{file_id}.pkl'):
+                    with open(f'{models_dir}/aim_model_{file_id}.pkl', 'rb') as f:
+                        aim_model_graph = dill.load(f)
+                        workload = selectTargetMarginals(pgm_train_df.columns, target, mode=PGMmarginals)
+                        workload = {(m, 1.0) for m in workload}
+                else:
+                    # 1) get AIM model and save it
 
-                    file_id = f'{dataset}_epsilon{epsilon}_delta{delta}_nlimit{n_limit}_t{t}_ymrgs{aim_y_mrg_opt}'
-                    if os.path.exists(f'models/aim_model_{file_id}.pkl'):
-                        with open(f'models/aim_model_{file_id}.pkl', 'rb') as f:
-                            aim_model_graph = dill.load(f)
-                            workload = selectTargetMarginals(pgm_train_df.columns, target, mode=PGMmarginals)
-                            workload = {(m, 1.0) for m in workload}
-                    else:
-                        # 1) get AIM model and save it
+                    initial_cliques = "y_marginals" if aim_y_mrg_opt else None
 
-                        initial_cliques = "y_marginals" if aim_y_mrg_opt else None
+                    aim_model_graph, workload = get_aim_model(pgm_train_df, domain, target, PGMmarginals, epsilon, delta, model_size,
+                                                        max_iters, len(X), initial_cliques)
 
-                        aim_model_graph, workload = get_aim_model(pgm_train_df, domain, target, PGMmarginals, epsilon, delta, model_size,
-                                                            max_iters, len(X), initial_cliques)
+                    with open(f'{models_dir}/aim_model_{file_id}.pkl', 'wb') as f:
+                        dill.dump(aim_model_graph, f)
 
-                        with open(f'models/aim_model_{file_id}.pkl', 'wb') as f:
-                            dill.dump(aim_model_graph, f)
+                    # 2) load AIM model and get marginal tables and synthetic data X_synth, y_synth
+                    with open(f'{models_dir}/aim_model_{file_id}.pkl', 'rb') as f:
+                        aim_model_graph = dill.load(f)
 
-                        # 2) load AIM model and get marginal tables and synthetic data X_synth, y_synth
-                        with open(f'models/aim_model_{file_id}.pkl', 'rb') as f:
-                            aim_model_graph = dill.load(f)
+                aim_ans_wkld = {cl: aim_model_graph.project(cl) for cl, _ in workload}
+                W = {key: aim_ans_wkld[key].__dict__['values'] for key in aim_ans_wkld}
 
-                    aim_ans_wkld = {cl: aim_model_graph.project(cl) for cl, _ in workload}
-                    W = {key: aim_ans_wkld[key].__dict__['values'] for key in aim_ans_wkld}
+                synth = aim_model_graph.synthetic_data(rows=n).df
+                synth_X, synth_y = synth.loc[:, synth.columns != target], synth.loc[:, synth.columns == target]
 
-                    synth = aim_model_graph.synthetic_data(rows=n).df
-                    synth_X, synth_y = synth.loc[:, synth.columns != target], synth.loc[:, synth.columns == target]
+                # AIM query ss
+                W_expanded = expand_W(W, attribute_dict)
 
-                    # AIM query ss
-                    W_expanded = expand_W(W, attribute_dict)
+                # approximate sufficient statistics
+                all_attributes_expanded = all_columns.append(pd.Index([target]))
+                ZTZ = get_ZTZ(W_expanded, attribute_dict, all_attributes_expanded, features_to_encode, target, one_hot, True)
+                cheb = Chebyshev(-6, 6, 3, phi_logit)
+                theta_dpqueryss = dp_query_approx_ss_logreg(ZTZ, all_columns, target, n, cheb, C=1.0)
+                # y_dpqueryss = dp_query_approx_ss_logreg(ZTZ, all_columns, target, n, cheb, C=1.0).predict(X_test)
+                # dpqueryss_auc = roc_auc_score(y_test, probabilities)
+                # print(y_dpqueryss)
 
-                    # approximate sufficient statistics
-                    all_attributes_expanded = all_columns.append(pd.Index([target]))
-                    ZTZ = get_ZTZ(W_expanded, attribute_dict, all_attributes_expanded, features_to_encode, target, one_hot, scale_y)
-                    cheb = Chebyshev(-6, 6, 3, phi_logit)
-                    theta_dpqueryss = dp_query_approx_ss_logreg(ZTZ, all_columns, target, n, cheb, C=1.0)
+                logits = np.dot(X_test, theta_dpqueryss)
+                probabilities = 1 / (1 + np.exp(-logits))
+                dpqueryss_auc = roc_auc_score(y_test, probabilities)
 
-                    logits = np.dot(X_test, theta_dpqueryss)
-                    probabilities = 1 / (1 + np.exp(-logits))
-                    dpqueryss_auc = roc_auc_score(y_test, probabilities)
+                res_out.append([dataset, "aim_ss_init"+str(aim_y_mrg_opt), dpqueryss_auc, t, seed, n_limit, None, epsilon, delta])
+                print(f"dpqueryss auc: {dpqueryss_auc}")
 
-                    res_out.append([dataset, "aim_ss_init"+str(aim_y_mrg_opt), dpqueryss_auc, t, seed, n_limit, None, epsilon, delta])
-                    print(f"dpqueryss auc: {dpqueryss_auc}")
+                # AIM synth
+                if one_hot:
+                    synth_X = one_hot_encode(synth_X, features_to_encode, attribute_dict)
+                    synth_X = normalize_minus1_1(synth_X, attribute_dict, encoded_features)
 
-                    # AIM synth
-                    if one_hot:
-                        synth_X = one_hot_encode(synth_X, features_to_encode, attribute_dict)
-                        synth_X = normalize_minus1_1(synth_X, attribute_dict, encoded_features)
-                        if scale_y:
-                            synth_y = normalize_minus1_1(synth_y, attribute_dict, encoded_features)
+                synth_y = normalize_minus1_1(synth_y, attribute_dict, encoded_features)
 
-                    zero_std_cols = []
-                    for col in synth_X.columns:
-                        if np.std(synth_X[col]) == 0:
-                            print(
-                                f"feature {col} is a zero vector! Dropping it at train time, adding corresponding zeros in theta")
-                            zero_std_cols.append(col)
-                    synth_X.drop(columns=zero_std_cols, inplace=True)
+                zero_std_cols = []
+                for col in synth_X.columns:
+                    if np.std(synth_X[col]) == 0:
+                        print(
+                            f"feature {col} is a zero vector! Dropping it at train time, adding corresponding zeros in theta")
+                        zero_std_cols.append(col)
+                synth_X.drop(columns=zero_std_cols, inplace=True)
 
-                    theta_aimsynth = public_linreg(synth_X, synth_y)
-                    for col in zero_std_cols:
-                        theta_aimsynth.loc[col] = 0
+                theta_aimsynth = public_logreg(synth_X, synth_y, all_columns)
+                for col in zero_std_cols:
+                    theta_aimsynth.loc[col] = 0
 
-                    theta_aimsynth = theta_aimsynth.reindex(index=all_columns, fill_value=0)
+                theta_aimsynth = theta_aimsynth.reindex(index=all_columns, fill_value=0)
 
-                    for i, col in enumerate(all_columns):
-                        assert col == theta_aimsynth.index[i]
+                for i, col in enumerate(all_columns):
+                    assert col == theta_aimsynth.index[i]
 
-                    theta_aimsynth = theta_aimsynth.to_numpy()
+                theta_aimsynth = theta_aimsynth.to_numpy()
 
-                    aimsynth_auc = testLogReg(theta_aimsynth, X_test, y_test)
-                    print(f"aim synth auc: {aimsynth_auc}")
-                    res_out.append([dataset, "aim_init"+str(aim_y_mrg_opt), aimsynth_auc, t, seed, n_limit, None, epsilon, delta])
+                aimsynth_auc = testLogReg(theta_aimsynth, X_test, y_test)
+                res_out.append([dataset, "aim_init"+str(aim_y_mrg_opt), aimsynth_auc, t, seed, n_limit, None, epsilon, delta])
+                print("aim synth auc", aimsynth_auc)
 
-                    # PGM direct prediction
-                    G = aim_model_graph
-                    target_levels = attribute_dict[target]
-                    G_y = X_test_pre.apply(pred_y_given_x_from_G, axis=1, args=(G, target, target_levels))
-                    G_y = G_y.to_frame(name=target)
-                    print(y_test[:10], G_y[:10])
-                    print(stop)
-                    #G_fpr, G_tpr, G_threshold = roc_curve(y_test, G_y)
-                    G_auc_1 = roc_auc_score(y_test, G_y)
-                    G_auc_2 = roc_auc_score(y_test, G_y.apply(lambda x: 1-x))
-                    G_auc = max(G_auc_1, G_auc_2)
-                    print("G auc", G_auc)
-                    res_out.append([dataset, "aim_G_init"+str(aim_y_mrg_opt), G_auc, t, seed, n_limit, None, epsilon,
-                         delta])
+                # PGM direct prediction
+                G = aim_model_graph
+                target_levels = attribute_dict[target]
+                G_y_probabilities = X_test_pre.apply(pred_y_given_x_from_G, axis=1, args=(G, target, target_levels, 'logistic'))
+                G_y_probabilities = np.stack(G_y_probabilities)
+                G_auc = roc_auc_score(y_test, G_y_probabilities[:,1])
+                print("G auc", G_auc)
+                res_out.append([dataset, "aim_G_init"+str(aim_y_mrg_opt), G_auc, t, seed, n_limit, None, epsilon,
+                     delta])
 
             out_df = pd.DataFrame(res_out, columns=col_out)
             one_hot_flag_str = '' if not one_hot else 'one-hot_True'

utils.py
@@ -27,7 +27,7 @@ from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.metrics import f1_score, roc_curve, auc, accuracy_score, confusion_matrix, roc_auc_score, mean_squared_error
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.model_selection import GridSearchCV
-from scipy.special import softmax, expit
+from scipy.special import softmax, expit, logsumexp
 from scipy.optimize import minimize, fmin_bfgs, fmin_tnc
 from scipy import sparse
 from absl import flags
@@ -246,7 +246,6 @@ def balance_target_pairs(out, target):
 
     for b in range(0, total_len):  # We start from 1 since b can't be 0
         current_diff = abs((y_marg_len + b * y_marg_len) - p * total_len)  # we want 50% target marginals
-        print("b", b, "current diff", current_diff)
         if current_diff < previous_diff:
             best_b = b
             previous_diff = current_diff
@@ -293,8 +292,6 @@ def normalize_minus1_1(X, attribute_dict, encoded_features):
 
         # for all other features, rescale to [-1, 1]
         else:
-            print(original_ranges)
-            print(col)
             colmin = original_ranges[col][0]
             colmax = original_ranges[col][1]
 
@@ -580,7 +577,7 @@ class SSApproxLL():
         z = np.dot(X, self.theta.T)
         y_pred_proba = expit(z)
         y_pred = 2 * ((y_pred_proba >= threshold).astype(int)) - 1
-        print("y pred dpapproxss", y_pred)
+        print("y pred ss", y_pred)
         return (y_pred)
 
 def get_aim_model(pgm_train_df, domain, target, marginals_pgm, epsilon, delta, model_size, max_iters, n_samples, initial_cliques):
@@ -595,42 +592,86 @@ def get_aim_model(pgm_train_df, domain, target, marginals_pgm, epsilon, delta, m
     return aim_model, mrgs_wkld
 
 
-def get_unnormalized_prob(x, potentials):
-    #print("potentials", potentials)
+def get_unnormalized_prob(data, potentials):
+    """
+    Parameters
+    data: x and y data (dataframe)
+    potentials: AIM model potentials ({factor: potential} dictionary)
+
+    Returns
+    sum_potentials: raw sum of potentials for x_and_y
+    """
+
     sum_potentials = 0
+
     for factor in potentials:
-        #print("factor", factor)
-        #print("potentials[factor]", potentials[factor])
-        index = tuple([int(x[attr]) for attr in factor])
-        #print("index", index)
+        index = tuple([int(data[attr]) for attr in factor])
         factor_value = potentials[factor].values[index]
-        #print("factor_value", factor_value)
         sum_potentials += factor_value
 
     return sum_potentials
 
 
-def pred_y_given_x_from_G(x, G, target, target_levels):
+def pred_y_given_x_from_G(x, G, target, target_levels, model):
+    """
+    Parameters
+    x: 1xd feature vector for one data point (dataframe)
+    G: graphical model output by AIM (dictionary)
+    target: name of target variable (str)
+    target_levels: all possible values the target variable can take (list)
+
+    Returns
+    best_guess: y value that maximized the unnormalized probability (int)
+    """
+    # get the model potentials
     potentials = G.potentials
     unnormalized_probs = {}
 
-    # print("potentials", potentials)
-    # print("target_levels", target_levels)
-
     for level in range(len(target_levels)):
-        # print("level", level)
         x_and_y = x.copy()
         x_and_y[target] = level
         unnormalized_probs[level] = get_unnormalized_prob(x_and_y, potentials)
-        # print("unnormalized_probs[level]", unnormalized_probs[level])
 
-    list_guesses = list(unnormalized_probs.items())
-    list_guesses.sort(key=lambda x: x[1])
-    best_guess = list_guesses[-1][0]
+    # print(unnormalized_probs)
+    # exp_unnorm_probs = {k: np.exp(unnormalized_probs[k]) for k in unnormalized_probs}
+    # tot = np.sum(list(exp_unnorm_probs.values()))
+    #
+    # print(exp_unnorm_probs)
+    #
+    # norm_probs = {k: exp_unnorm_probs[k]/tot for k in exp_unnorm_probs}
+    #
+    # # for k in norm_probs:
+    # #     if np.isnan(norm_probs[k]):
+    # #         print("HERE")
+    # #         norm_probs = {el: 1/len(norm_probs) for el in norm_probs}
+    #
+    # print(norm_probs)
 
-    return best_guess
+    log_probs = list(unnormalized_probs.values())
+    log_sum_exp = logsumexp(log_probs)
+    normalized_log_probs = log_probs - log_sum_exp
+
+    # exp to get normalized probabilities
+    norm_probs = np.exp(normalized_log_probs)
+    keys = list(unnormalized_probs.keys())
+    norm_probs = dict(zip(keys, norm_probs))
+
+    # pick y value that gives the maximum unnormalized prop
+    if model == 'logistic':
+        probabilities = list(norm_probs.values())
+        return probabilities
+
+    elif model == 'linear':
+        list_guesses = norm_probs.items()
+        guess = np.sum([g[0] * g[1] for g in list_guesses])
+        return guess
+
+    else:
+        print("method not implemented!")
+        return
 
 def testLogReg(theta, X_test, y_test):
+    print(y_test)
     logits = np.dot(X_test, theta)
     probabilities = 1 / (1 + np.exp(-logits))
     auc = roc_auc_score(y_test, probabilities)
@@ -642,6 +683,7 @@ def testLinReg(theta, X_test, y_test):
     return mse
 
 def public_logreg(X, y, all_columns):
+    print(all_columns)
     model = LogisticRegression(penalty=None, fit_intercept=False, max_iter=2000)
     model.fit(X.to_numpy(), y.to_numpy().ravel())
     theta = model.coef_.ravel()   # probabilities for class 1