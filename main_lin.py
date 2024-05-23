8832493231cf9bc7582ebbc469ef82e4db7ae726
Cecilia Ferrando <33758082+ceciliaferrando@users.noreply.github.com>
2024-05-23 12:31:20 -0400

    Update main_lin.py

--- main_lin.py
+++ main_lin.py
@@ -1,54 +1,21 @@
-import math
-import numpy as np
-from scipy import sparse
-import pandas as pd
-import copy
-from datetime import datetime
-from itertools import combinations
-import os
 import argparse
-import seaborn as sns
-import matplotlib.pyplot as plt
-import json
-import pdb
-import itertools
-import time
-import argparse
-import warnings
-import pickle
 import dill
-
 import sys
 sys.path.append('..')
+import warnings
+warnings.filterwarnings("ignore")
 
-# from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression, LinearRegression
-from sklearn.metrics import f1_score, roc_curve, auc, accuracy_score, confusion_matrix, roc_auc_score, mean_squared_error
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.model_selection import GridSearchCV
-from scipy.special import softmax, expit
-from scipy.optimize import minimize, fmin_bfgs, fmin_tnc
-from scipy import sparse
-from absl import flags
-from datetime import datetime
-from itertools import combinations
-from tqdm.auto import tqdm
-
-from private_pgm_local.src.mbi import Dataset, FactoredInference
-from diffprivlib_main.diffprivlib_local import models as dp
-from private_pgm_local.mechanisms import aim
-from private_pgm_local.src.mbi.workload import Workload
-
+from ddssp import *
 from utils import *
+from baselines import *
 
 parser = argparse.ArgumentParser(description='Experiment Inputs')
-parser.add_argument('--dataset', help='Dataset', type=str, default='fire')
+parser.add_argument('--dataset', help='Dataset', type=str, default='adult')
 parser.add_argument('--method', help='Method to be used', type=str, default='aim',
                     choices=['public', 'adassp', 'aim'])
 parser.add_argument('--delta', type=float, default=1e-5)
 parser.add_argument('--num_experiments', type=int, default=5)
-parser.add_argument('--seed', type=int, default=22)
+parser.add_argument('--seed', type=int, default=242)
 parser.add_argument('--n_limit', type=int, default=50000)
 parser.add_argument('--one_hot', type=str, default='True')
 parser.add_argument('--scale_y', type=str, default='True')
@@ -64,22 +31,23 @@ seed = args.seed
 n_limit = args.n_limit
 one_hot = True if args.one_hot == 'True' else False
 scale_y = True if args.scale_y == 'True' else False
-#aim_y_mrg_opt = True if args.aim_y_mrg_opt == 'True' else None
+aim_y_mrg_opt = True if args.aim_y_mrg_opt == 'True' else None
 
 if __name__ == "__main__":
 
-    # AIM model parameters
+    ##### AIM model parameters  ########################################################################################
     model_size = 200
     max_iters = 1000
     PGMmarginals = 'all-pairs'
 
-    target_dict = {'adult': 'education-num', 'taxi': 'totalamount', 'fire': 'Priority', 'ACSincome-LIN': 'PINCP',
-                   'ACSPublicCoverage': 'AGEP', 'ACSmobility': 'AGEP', 'linregbinary': 'predicted',
-                   'linregbinary10': 'predicted',
-                   'linregbinary30': 'predicted'}
+    ##### Setup  #######################################################################################################
 
     np.random.seed(seed)
 
+    target_dict = {'adult': 'education-num', 'taxi': 'totalamount', 'fire': 'Priority', 'ACSincome-LIN': 'PINCP',
+                   'ACSPublicCoverage': 'AGEP', 'ACSmobility': 'AGEP', 'linregbinary': 'predicted',
+                   'linregbinary10': 'predicted',
+                   'linregbinary30': 'predicted'} 
 
     (X, X_test, y, y_test, X_pre, X_test_pre,
      pgm_train_df, domain, target,
@@ -87,13 +55,7 @@ if __name__ == "__main__":
      original_ranges, all_columns, zero_std_cols) = preprocess_data(dataset, target_dict, n_limit,
                                                                     one_hot, scale_y)
 
-
     n, d = X.shape
-
-    print(f"X.shape {X.shape}")
-    print(f"X_test.shape {X_test.shape}")
-    print(f"y.shape {y.shape}")
-    print(f"y_test.shape {y_test.shape}")
 
     res_out = []
     col_out = ['dataset', 'method', 'mse', 'r2', 'experiment_n', 'seed', 'n_limit',
@@ -101,9 +63,9 @@ if __name__ == "__main__":
     outdir = dataset + "_linreg/"
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-
     models_dir = "models_linear"
 
+    ##### Experiment over t trials  ####################################################################################
 
     for t in range(num_experiments):
 
@@ -111,19 +73,24 @@ if __name__ == "__main__":
 
             print(f'epsilon: {epsilon}, t: {t}')
 
-            # bounds
+            ##### bounds  ##############################################################################################
             bound_XTX, bound_X = get_bound_XTX(attribute_dict, target, one_hot)
             bound_y = 1 if one_hot else np.abs(np.max(attribute_dict[target]))
 
             XTX_public = pd.DataFrame(np.dot(X.T, X), index=X.columns, columns=X.columns)
             XTy_public = pd.DataFrame(np.dot(X.T, y), index=X.columns)
 
+            ##### public  ##############################################################################################
+
             if method == 'public':
                 theta_public = public_linreg(X, y)
                 theta_public = theta_public.reindex(index=all_columns, fill_value=0)
                 mse_public = testLinReg(theta_public, X_test, y_test)
+                print(f"public mse {mse_public}")
                 res_out.append([dataset, method, mse_public, None, t, seed,
                                 n_limit, None, epsilon, delta])
+
+            ##### adassp  ##############################################################################################
 
             elif method == 'adassp':
                 XTy_budget_split = 0.05
@@ -137,97 +104,82 @@ if __name__ == "__main__":
 
                 XTX_dp_error, XTy_dp_error = get_SS_mse(XTX_dp_reg, XTX_public,
                                                           XTy_dp, XTy_public)
-
+                print(f"adassp mse {mse_adassp}")
                 res_out.append([dataset, method, mse_adassp, None, t, seed,
                                 n_limit, None, epsilon, delta])
 
+            ##### DDSSP & AIM Synth  ###################################################################################
+
             elif method == 'aim':
 
-                for aim_y_mrg_opt in [False]:
+                file_id = f'{dataset}_epsilon{epsilon}_delta{delta}_nlimit{n_limit}_seed{seed}_t{t}'
+                if os.path.exists(f'{models_dir}/aim_model_{file_id}.pkl'):
+                    with open(f'{models_dir}/aim_model_{file_id}.pkl', 'rb') as f:
+                        aim_model_graph = dill.load(f)
+                        mrgs = selectTargetMarginals(pgm_train_df.columns, target, mode=PGMmarginals)
+                        workload = {(m, 1.0) for m in mrgs}
+                else:
+                    # 1) get AIM model and save it
+                    initial_cliques = "y_marginals" if aim_y_mrg_opt else None
+                    aim_model_graph, workload = get_aim_model(pgm_train_df, domain, target, PGMmarginals, epsilon, delta, model_size,
+                                                        max_iters, len(X), initial_cliques)
 
-                    file_id = f'{dataset}_epsilon{epsilon}_delta{delta}_nlimit{n_limit}_t{t}_aimopt{aim_y_mrg_opt}'
-                    if os.path.exists(f'{models_dir}/aim_model_{file_id}.pkl'):
-                        with open(f'{models_dir}/aim_model_{file_id}.pkl', 'rb') as f:
-                            aim_model_graph = dill.load(f)
-                            mrgs = selectTargetMarginals(pgm_train_df.columns, target, mode=PGMmarginals)
-                            workload = {(m, 1.0) for m in mrgs}
-                    else:
-                        # 1) get AIM model and save it
+                    with open(f'{models_dir}/aim_model_{file_id}.pkl', 'wb') as f:
+                        dill.dump(aim_model_graph, f)
 
-                        initial_cliques = "y_marginals" if aim_y_mrg_opt else None
+                    # 2) load AIM model and get marginal tables and synthetic data X_synth, y_synth
+                    with open(f'{models_dir}/aim_model_{file_id}.pkl', 'rb') as f:
+                        aim_model_graph = dill.load(f)
 
-                        aim_model_graph, workload = get_aim_model(pgm_train_df, domain, target, PGMmarginals, epsilon, delta, model_size,
-                                                            max_iters, len(X), initial_cliques)
+                aim_ans_wkld = {cl: aim_model_graph.project(cl) for cl, _ in workload}
+                W = {key: aim_ans_wkld[key].__dict__['values'] for key in aim_ans_wkld}
 
-                        with open(f'{models_dir}/aim_model_{file_id}.pkl', 'wb') as f:
-                            dill.dump(aim_model_graph, f)
+                synth = aim_model_graph.synthetic_data(rows=n).df
+                synth_X, synth_y = synth.loc[:, synth.columns != target], synth.loc[:, synth.columns == target]
 
-                        # 2) load AIM model and get marginal tables and synthetic data X_synth, y_synth
-                        with open(f'{models_dir}/aim_model_{file_id}.pkl', 'rb') as f:
-                            aim_model_graph = dill.load(f)
+                # DDSSP
+                W_expanded = expand_W(W, attribute_dict)
+                all_attributes_expanded = all_columns.append(pd.Index([target]))
+                ZTZ = get_ZTZ(W_expanded, attribute_dict, all_attributes_expanded, features_to_encode, target, one_hot, scale_y)
+                theta_dpqueryss, XTX_dpqueryss, XTy_dpqueryss = dp_query_ss_linreg(ZTZ, all_columns, target)
 
-                    print(workload)
-                    aim_ans_wkld = {cl: aim_model_graph.project(cl) for cl, _ in workload}
-                    W = {key: aim_ans_wkld[key].__dict__['values'] for key in aim_ans_wkld}
+                mse_dpqueryss = testLinReg(theta_dpqueryss, X_test, y_test)
+                print(f"aim ss mse {mse_dpqueryss}")
+                res_out.append([dataset, "aim_ss", mse_dpqueryss, None, t, seed,
+                                n_limit, None, epsilon, delta])
 
-                    synth = aim_model_graph.synthetic_data(rows=n).df
-                    synth_X, synth_y = synth.loc[:, synth.columns != target], synth.loc[:, synth.columns == target]
-
-                    # AIM query ss
-                    W_expanded = expand_W(W, attribute_dict)
-
-                    # approximate sufficient statistics
-                    all_attributes_expanded = all_columns.append(pd.Index([target]))
-                    ZTZ = get_ZTZ(W_expanded, attribute_dict, all_attributes_expanded, features_to_encode, target, one_hot, scale_y)
-                    theta_dpqueryss, XTX_dpqueryss, XTy_dpqueryss = dp_query_ss_linreg(ZTZ, all_columns, target)
-
-                    mse_dpqueryss = testLinReg(theta_dpqueryss, X_test, y_test)
-                    print(f"aim ss mse {mse_dpqueryss}")
-                    res_out.append([dataset, "aim_ss_init"+str(aim_y_mrg_opt), mse_dpqueryss, None, t, seed,
-                                    n_limit, None, epsilon, delta])
-
-                    # AIM synth
-                    if one_hot:
-                        synth_X = one_hot_encode(synth_X, features_to_encode, attribute_dict)
-                        synth_X = normalize_minus1_1(synth_X, attribute_dict, encoded_features)
-                        if scale_y:
-                            synth_y = normalize_minus1_1(synth_y, attribute_dict, encoded_features)
-
-                    zero_std_cols = []
-                    for col in synth_X.columns:
-                        if np.std(synth_X[col]) == 0:
-                            print(
-                                f"feature {col} is a zero vector! Dropping it at train time, adding corresponding zeros in theta")
-                            zero_std_cols.append(col)
-                    synth_X.drop(columns=zero_std_cols, inplace=True)
-
-                    theta_aimsynth = public_linreg(synth_X, synth_y)
-                    for col in zero_std_cols:
-                        theta_aimsynth.loc[col] = 0
-
-                    theta_aimsynth = theta_aimsynth.reindex(index=all_columns, fill_value=0)
-
-                    for i, col in enumerate(all_columns):
-                        assert col == theta_aimsynth.index[i]
-
-                    theta_aimsynth = theta_aimsynth.to_numpy()
-
-                    mse_aimsynth = testLinReg(theta_aimsynth, X_test, y_test)
-                    print(f"aim synth mse {mse_aimsynth}")
-                    res_out.append([dataset, "aim_init"+str(aim_y_mrg_opt), mse_aimsynth, None, t, seed,
-                                    n_limit, None, epsilon, delta])
-
-                    # PGM direct prediction
-                    G = aim_model_graph
-                    target_levels = attribute_dict[target]
-                    G_y = X_test_pre.apply(pred_y_given_x_from_G, axis=1, args=(G, target, target_levels, 'linear'))
-                    G_y = G_y.to_frame(name=target)
+                # AIM synth
+                if one_hot:
+                    synth_X = one_hot_encode(synth_X, features_to_encode, attribute_dict)
+                    synth_X = normalize_minus1_1(synth_X, attribute_dict, encoded_features)
                     if scale_y:
-                        G_y = normalize_minus1_1(G_y, attribute_dict, encoded_features)
-                    G_mse = mean_squared_error(y_test, G_y)
-                    print("G mse", G_mse)
-                    res_out.append([dataset, "aim_G_init" + str(aim_y_mrg_opt), G_mse, None, t, seed,
-                                    n_limit, None, epsilon, delta])
+                        synth_y = normalize_minus1_1(synth_y, attribute_dict, encoded_features)
+
+                zero_std_cols = []
+                for col in synth_X.columns:
+                    if np.std(synth_X[col]) == 0:
+                        print(
+                            f"feature {col} is a zero vector! Dropping it at train time, adding corresponding zeros in theta")
+                        zero_std_cols.append(col)
+                synth_X.drop(columns=zero_std_cols, inplace=True)
+
+                theta_aimsynth = public_linreg(synth_X, synth_y)
+                for col in zero_std_cols:
+                    theta_aimsynth.loc[col] = 0
+
+                theta_aimsynth = theta_aimsynth.reindex(index=all_columns, fill_value=0)
+
+                for i, col in enumerate(all_columns):
+                    assert col == theta_aimsynth.index[i]
+
+                theta_aimsynth = theta_aimsynth.to_numpy()
+
+                mse_aimsynth = testLinReg(theta_aimsynth, X_test, y_test)
+                print(f"aim synth mse {mse_aimsynth}")
+                res_out.append([dataset, "aim", mse_aimsynth, None, t, seed,
+                                n_limit, None, epsilon, delta])
+
+            ##### progressively save  ##################################################################################
 
             out_df = pd.DataFrame(res_out, columns=col_out)
             filename_out = f'{outdir}{dataset}_{method}_{epsilon}_{t}_{num_experiments}exps_{n_limit}limit_' \
